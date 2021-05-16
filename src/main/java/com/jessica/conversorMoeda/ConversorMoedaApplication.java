ea921b77cdea1048e55f94db549e599f05fc34a4
jessica <jessica.seibert@impa.br>
2021-05-16 15:38:59 -0300

    iniciando o projeto

---
+++ src/main/java/com/jessica/conversorMoeda/ConversorMoedaApplication.java
@@ -0,0 +1,13 @@
+package com.jessica.conversorMoeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorMoedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorMoedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessica/conversorMoeda/ConversorMoedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessica.conversorMoeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorMoedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
